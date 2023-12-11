c2dd3409a8ef05ead2e39c192340d294652b878d
Thomas Santosa <thomassantosa07@gmail.com>
2023-12-11 18:34:35 +0700

    add-testapiservice

--- Smart Harvest App/app/src/main/java/com/example/smartharvest/data/api/ApiConfig.kt
+++ Smart Harvest App/app/src/main/java/com/example/smartharvest/data/api/ApiConfig.kt
@@ -19,4 +19,18 @@ class ApiConfig {
             .build()
         return retrofit.create(ApiService::class.java)
     }
+
+    fun getTestApiService(): TestApiService {
+        val loggingInterceptor =
+            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+        val client = OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://story-api.dicoding.dev/v1/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+        return retrofit.create(TestApiService::class.java)
+    }
 }
--- Smart Harvest App/app/src/main/java/com/example/smartharvest/data/api/TestApiService.kt
+++ Smart Harvest App/app/src/main/java/com/example/smartharvest/data/api/TestApiService.kt
@@ -10,21 +10,10 @@ import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.POST
-import retrofit2.http.Query
 
-interface ApiService {
+interface TestApiService {
 
-    @POST("register")
-    fun registerUser(
-        @Body request: RegisterRequest,
-    ): Call<RegisterResponse>
-
-    @POST("login")
-    fun loginUser(
-        @Body request: LoginRequest,
-    ): Call<LoginResponse>
-
-    @GET("products-catalog")
+    @GET("stories")
     suspend fun getStories(
         @Header("Authorization") token: String
     ): ProductCatalogResponse
