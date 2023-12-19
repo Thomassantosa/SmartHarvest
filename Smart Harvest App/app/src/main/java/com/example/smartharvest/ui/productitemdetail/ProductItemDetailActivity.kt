6dabe384fdcf86b0db797d68e59362c4475c4afd
Thomas Santosa <thomassantosa07@gmail.com>
2023-12-19 20:47:59 +0700

    update-detail-activity

--- Smart Harvest App/app/src/main/java/com/example/smartharvest/ui/productitemdetail/ProductItemDetailActivity.kt
+++ Smart Harvest App/app/src/main/java/com/example/smartharvest/ui/productitemdetail/ProductItemDetailActivity.kt
@@ -1,12 +1,64 @@
 package com.example.smartharvest.ui.productitemdetail
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
-import com.example.smartharvest.R
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.example.smartharvest.data.responses.DataItem
+import com.example.smartharvest.databinding.ActivityProductItemDetailBinding
+import java.text.NumberFormat
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.Locale
 
 class ProductItemDetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityProductItemDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_product_item_detail)
+        binding = ActivityProductItemDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupData()
+    }
+
+    private fun setupData() {
+        val productItem = intent.getParcelableExtra<DataItem>("ProductItem") as DataItem
+        Glide.with(applicationContext)
+            .load(productItem.photoUrl)
+            .into(binding.imgProductDetail)
+
+        // Number format
+        val indonesianLocale = Locale("in", "ID")
+        val numberFormat = NumberFormat.getInstance(indonesianLocale)
+        val formattedItemPrice = numberFormat.format(productItem.priceSeller)
+        val formattedNationalPrice = numberFormat.format(productItem.nationalPrice)
+        val formattedPredictionPrice = numberFormat.format(productItem.predictionPrice)
+
+        // Date format
+        val formattedDateString: String
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val inputFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+            val outputFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+
+            val dateTimeString = productItem.harvestDate
+            val parsedHarvestDate = LocalDateTime.parse(dateTimeString, inputFormat)
+            formattedDateString = parsedHarvestDate.format(outputFormat)
+        } else {
+            formattedDateString = productItem.harvestDate.toString()
+        }
+
+        binding.apply {
+            textItemName.text = productItem.name
+            textItemPrice.text = "IDR " + formattedItemPrice.toString()
+            textNationalPrice.text = "IDR " + formattedNationalPrice.toString()
+            textPredictionPrice.text = "IDR " + formattedPredictionPrice.toString()
+            textProducer.text = productItem.producerName
+            textHarvestDate.text = formattedDateString
+            textDistributor.text = productItem.distributorName
+            textSeller.text = productItem.sellerName
+            textDescValue.text = productItem.description
+        }
     }
 }
