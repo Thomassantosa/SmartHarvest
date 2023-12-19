e9a1ed409b33fe665eaa372d845473b9104cc06e
taufiqarinta <taufiqardiyono33@gmail.com>
2023-12-20 02:47:32 +0700

    update-android-profile-activity-1

--- Smart Harvest App/app/src/main/java/com/example/smartharvest/ui/profile/ProfileActivity.kt
+++ Smart Harvest App/app/src/main/java/com/example/smartharvest/ui/profile/ProfileActivity.kt
@@ -3,10 +3,28 @@ package com.example.smartharvest.ui.profile
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.smartharvest.R
+import com.example.smartharvest.databinding.ActivityLoginBinding
+import com.example.smartharvest.databinding.ActivityProfileBinding
+import com.example.smartharvest.ui.login.LoginViewModel
 
 class ProfileActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityProfileBinding
+    private lateinit var profileViewModel: ProfileViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        profileViewModel.getUser().observe(this) {user ->
+            if (user.token.isNotEmpty()) {
+                binding.nameProfile.text = user.name
+                binding.typeProfile.text = user.type
+            }
+        }
+
     }
 }
---
+++ Smart Harvest App/app/src/main/java/com/example/smartharvest/ui/profile/ProfileViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.smartharvest.ui.profile
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.example.smartharvest.data.repository.Repository
+import com.example.smartharvest.data.responses.LoginResult
+
+class ProfileViewModel (private val repository: Repository) : ViewModel(){
+
+    fun getUser(): LiveData<LoginResult> {
+        return repository.getUser()
+    }
+}
